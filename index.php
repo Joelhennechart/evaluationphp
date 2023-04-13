64f32bbe99ac984db5806f08d7356683c0f83f2b
Joelhennechart <joelhen@hotmail.fr>
2023-04-13 14:39:30 +0200

    acceuil

--- index.php
+++ index.php
@@ -1,12 +1,37 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="fr">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Todo List & Bibliothéque Musicale</title>
+    <link rel="stylesheet" type="text/css" href="css/style.css">
 </head>
-<body>
-    
+<body class="background">
+    <!-- navbar header Titre + logo nom -->
+     <header class="navbar"> 
+	    <nav class="navbar-content container">
+			<h1 class="titre">
+			Todo List & Bibliothèque Musicale
+			</h1>
+        <!-- zone logo -->
+            <name class="name">
+                <div class="logo"><img src="image/logo.png" alt="logo">
+                </div>
+            </name> 
+        </nav>
+    </header>
+
+    <main class="main">
+    <?php
+
+    ?>
+
+    </main>
+    <footer class="footer">
+		<p>Copyright © 2023 Hennechart Joël 26120 montmeyran</p>
+		<p><a href="monfichier-klordno74juf21maota5aedgasw8pd.html" title="monfichier-klordno74juf21maota5aedgasw8pd.html" target="_blank">Mentions légales</a></p>
+	<br>
+	</footer>
 </body>
 </html>
